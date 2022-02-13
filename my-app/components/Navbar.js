365e462ab4713014074b3e1a97037dccaed5886b
Katherine <kath.gbl@gmail.com>
2022-02-13 14:39:57 +0100

    Used Link to create navigation in the application

---
+++ my-app/components/Navbar.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/users">
+            <a>Users</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
