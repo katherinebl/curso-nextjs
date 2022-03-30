8fdb7a9204c4ea70aff4d5272c0f3d0395fa6e54
Katherine <kath.gbl@gmail.com>
2022-03-30 21:42:18 +0200

    Fixed typo

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import Title from '../components/Title'
 import Footer from '../components/Footer'
 
